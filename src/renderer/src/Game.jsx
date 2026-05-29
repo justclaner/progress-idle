f8d9de4eaa3458d4bdc453d765405acbd0a19c4a
justclaner <justclaner@gmail.com>
2026-05-29 17:33:11 -0400

    added save loading

--- src/renderer/src/Game.jsx
+++ src/renderer/src/Game.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import { useState, useEffect, Context } from 'react';
+
+const debug = true;
 
 const Game = () => {
+  const [save, setSave] = useState({});
+
+  // Access save on loadup
+  useEffect(() => {
+    const loadSave = async () => {
+      const save = await window.store.get('save');
+      if (save) {
+        setSave(save);
+      }
+    };
+    loadSave();
+  }, []);
+
+  useEffect(() => {
+    if (debug) {
+      console.log('save was updated!');
+      console.log(save);
+    }
+  }, [save]);
+
   return <div>Game</div>;
 };
 
